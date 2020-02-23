a61ea6216b9565dea421f2aebf2e2033e8c8aed9
thetaepsilon <thetaepsilon@noreply.users.github.com>
2020-02-23 16:05:21 +0000

    ds2::arrays: [+] add vector_ref<T>

---
+++ pkg/ds2/arrays/vector_ref.hpp
@@ -0,0 +1,35 @@
+#pragma once
+#include <cstddef>
+#include "array_ref.hpp"
+
+/*
+In reference to the STL's std::vector,
+this is a non-owning reference to a runtime-sized array of objects,
+essentially a pointer to the array and it's length.
+*/
+
+namespace ds2::array {
+	template <typename T>
+	struct vector_ref {
+	private:
+		T* data;
+		std::size_t len;
+		constexpr inline vector_ref(T* data, std::size_t length):
+			data(data), len(length) {}
+	public:
+		// named "unsafe" to hint to the caller they should be very sure before using this.
+		// also makes it stand out in code more.
+		static constexpr inline vector_ref<T> from_unsafe(T* data, std::size_t length) {
+			return vector_ref<T>(data, length);
+		}
+
+		// support "type erasing" a compile-time sized array into a runtime sized one.
+		template <std::size_t N>
+		constexpr inline vector_ref(T (&array)[N]):
+			data(&array[0]), len(N) {}
+		template <std::size_t N>
+		constexpr inline vector_ref(const array_ref<T, N>& ref):
+			data(ref.unsafe()), len(N) {}
+	};
+}
+
