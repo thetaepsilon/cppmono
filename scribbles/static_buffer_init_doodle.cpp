3d07f87c1cb396353de527c3ae14f2a881fe1279
thetaepsilon <thetaepsilon@noreply.users.github.com>
2020-04-02 21:42:50 +0100

    [~] scribbles/static_buffer_init_doodle.cpp: some renames to make more sense.

--- scribbles/static_buffer_init_doodle.cpp
+++ scribbles/static_buffer_init_doodle.cpp
@@ -2,18 +2,18 @@
 #include <cstddef>
 #include <ds2/ppmacros/unique_name.h>
 
-constexpr inline size_t operator "" _static_length(const char*, size_t length) {
+constexpr inline size_t operator "" _length(const char*, size_t length) {
     return length;
 }
 
 
 
-#define __ds2_arrays_buffer_fromlit_inner(tmpname, T, len, s, R, n, f) \
+#define __ds2_arrays_buffer_fromlit_named(tmpname, T, len, s, R, n, f) \
 	constexpr T tmpname [ len ] = s;\
-	constexpr R n = f(tmpname, s ## _static_length)
+	constexpr R n = f(tmpname, s ## _length)
 
 #define __ds2_arrays_buffer_fromlit(T, len, s, R, n, f) \
-    __ds2_arrays_buffer_fromlit_inner(__ds2__ppmacros__unique_name(x, buffer_storage), T, len, s, R, n, f)
+    __ds2_arrays_buffer_fromlit_named(__ds2__ppmacros__unique_name(x, buffer_storage), T, len, s, R, n, f)
 
 
 template <typename T>
@@ -26,6 +26,12 @@ template <typename T, size_t N>
 constexpr inline auto buffer_from_array(T (&arr)[N], size_t used) {
 	return buffer_ref<T> { arr, N, used };
 }
+
+
+
+
+
+
 
 __ds2_arrays_buffer_fromlit(static char, 32, "asdf", extern const auto, wat, buffer_from_array);
 
