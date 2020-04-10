c72cddeda1ca6178e8480b8eabdc855e609e5d57
thetaepsilon <thetaepsilon@noreply.users.github.com>
2020-04-10 22:49:21 +0100

    scribbles/array apply: clean up the type signatures a bit, add a concat operation

--- scribbles/array_apply.cpp
+++ scribbles/array_apply.cpp
@@ -3,17 +3,7 @@ Constexpr array operations like concatenation (!),
 using a helper container around an array that calls a provided functor in it's constructor.
 */
 
-template <typename T, unsigned int L, typename F>
-struct array_apply {
-	T data[L];
 
-	// note we still have to specify data() even if we touch all array members,
-	// to satisfy the definition of a constexpr function.
-	// (untouched elements are default or zero initialised.)
-	constexpr inline array_apply(F f): data() {
-		f(data);
-	}
-};
 
 constexpr const int pattern[4] = {
 	0, 1, 2, 3
@@ -40,6 +30,50 @@ constexpr inline repeat<T, Ls, Rep> mk_repeat(const T (&src)[Ls]) {
 	return repeat<T, Ls, Rep>(src);
 }
 
-extern const array_apply<int, 64, repeat<int, 4, 16>> test;
-constexpr const array_apply<int, 64, repeat<int, 4, 16>> test(mk_repeat<16, int, 4>(pattern));
+
+
+template <typename T, unsigned int L>
+struct array_apply {
+	T data[L];
+
+	template <typename Functor>
+	constexpr inline array_apply(Functor&& f): data() {
+		f(data);
+	}
+};
+
+extern    const array_apply<int, 64> t1;
+constexpr const array_apply<int, 64> t1(mk_repeat<16, int, 4>(pattern));
+
+
+
+// woo, real lambda time
+template <typename T, unsigned int L1, unsigned int L2>
+constexpr auto concat(const T (&src1)[L1], const T (&src2)[L2]) {
+	return [&](T (&target)[L1+L2]) constexpr {
+		T* t = target;
+		unsigned int i = 0;
+		// constexpr complains even though we could set it here...
+		for (; i < L1; i++) {
+			*t++ = src1[i];
+		}
+		for (i = 0; i < L2; i++) {
+			*t++ = src2[i];
+		}
+	};
+}
+
+
+constexpr const int src1[2] = {0, 1};
+constexpr const int src2[4] = {0, 1, 2, 3};
+extern    const array_apply<int, 6> t2;
+constexpr const array_apply<int, 6> t2(concat(src1, src2));
+
+
+
+
+
+
+
+
 
