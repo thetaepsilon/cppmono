b8e3ff9ef58c20dbe2be080e73fc1cb19fb4e25a
thetaepsilon <thetaepsilon@noreply.users.github.com>
2020-10-21 15:06:42 +0100

    [+] scribbles: C++20 static inheritance constraint demo

---
+++ scribbles/static_inheritance_constraints.cpp
@@ -0,0 +1,32 @@
+#include <type_traits>
+
+template <bool b>
+constexpr std::enable_if<b>::type accept() {}
+
+// interesting, concepts are backwards from what you'd expect,
+// when used to constrain a template parameter.
+// the T being constrained always becomes the *first* argument!
+template <typename T, typename Parent>
+concept DerivedFrom = requires(T t) {
+	accept<std::is_base_of_v<Parent, T>>();
+};
+
+struct A {};
+struct B: public A {};
+
+static_assert(DerivedFrom<B, A>);
+
+
+template <DerivedFrom<A> T>
+constexpr bool foo() { return true; }
+
+static_assert(foo<B>());
+static_assert(not DerivedFrom<int, B>);
+// note that attempting to call foo<int>() is an instant hard unmet constraint error,
+// as we would not be reading the value directly, but negating the value of a function call,
+// where said function *always requires* DerivedFrom<T, A> to hold to even be valid.
+struct C {};
+static_assert(not DerivedFrom<C, B>);
+static_assert(not DerivedFrom<A, B>); // <- remember, backwards!
+
+
