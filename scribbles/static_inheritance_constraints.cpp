c5bb9c4ec6ab8d6ec9254ccecb8843aded3b2c26
thetaepsilon <thetaepsilon@noreply.users.github.com>
2020-10-22 16:52:40 +0100

    [+] static_inheritance_constraints.cpp: some comment cleanup and add a commented what-not-to-do example.

--- scribbles/static_inheritance_constraints.cpp
+++ scribbles/static_inheritance_constraints.cpp
@@ -12,11 +12,16 @@ constexpr bool foo() { return true; }
 
 static_assert(foo<B>());
 static_assert(not DerivedFrom<int, B>);
-// note that attempting to call foo<int>() is an instant hard unmet constraint error,
-// as we would not be reading the value directly, but negating the value of a function call,
-// where said function *always requires* DerivedFrom<T, A> to hold to even be valid.
+/*
+note that attempting to call foo<int>() is an instant hard unmet constraint error,
+as we would not be reading the value directly, but negating the value of a function call,
+where said function *always requires* DerivedFrom<T, A> to hold to even be valid.
+so the following line would be kaboom with "constraint(s) not satisfied" if uncommented:
+*/
+//static_assert(foo<int>());
+
 struct C {};
 static_assert(not DerivedFrom<C, B>);
-static_assert(not DerivedFrom<A, B>); // <- remember, backwards!
+static_assert(not DerivedFrom<A, B>);
 
 
