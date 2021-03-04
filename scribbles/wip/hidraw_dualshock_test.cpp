0eecba58cb16407efe844d5873e72b10d91639b6
thetaepsilon <thetaepsilon@noreply.users.github.com>
2021-03-04 14:25:47 +0000

    [* WIP] scribbles/hidraw_dualshock_test.cpp: packed struct rewrite

--- scribbles/wip/hidraw_dualshock_test.cpp
+++ scribbles/wip/hidraw_dualshock_test.cpp
@@ -10,8 +10,16 @@ extern "C" {
 #include <cstring>
 // uint8_t
 #include <cstdint>
+// CHAR_BIT
+#include <climits>
+// std::{make_signed, make_unsigned}
+#include <type_traits>
 
 typedef uint8_t u8;
+typedef uint16_t u16;
+
+typedef int16_t i16;
+typedef int8_t i8;
 
 
 inline int bail(const char* msg, size_t s) {
@@ -48,6 +56,8 @@ inline size_t hexline(u8* input, char* output, size_t count) {
 	return sz;
 }
 
+
+
 // supposedly, the C standard doesn't assume signed representation.
 // might as well be explicit about it here,
 // because the dualshock's IMU data most certainly *is* two's complement.
@@ -59,13 +69,6 @@ inline bool is_u8_negative(u8 v) {
 }
 inline uint16_t abs_from_signed_u16(uint16_t input) {
 	if (is_u16_negative(input)) {
-		return ((~input) + 1);
-	} else {
-		return input;
-	}
-}
-inline uint8_t abs_from_signed_u8(uint8_t input) {
-	if (is_u8_negative(input)) {
 		return ((~input) + 1);
 	} else {
 		return input;
@@ -86,12 +89,14 @@ inline void show_s16le(const u8* data, char* buf6) {
 	tohex(v >> 8, &buf6[2]);
 	tohex(v & 0xFF, &buf6[4]);
 }
-inline void show_s8le(const u8 uv, char* buf4) {
-	buf4[0] = ' ';
-	buf4[1] = ((is_u8_negative(uv)) ? '-' : '+');
-	u8 v = abs_from_signed_u8(uv);
-	tohex(v, &buf4[2]);
-}
+
+
+
+
+
+
+
+
 
 
 
@@ -112,24 +117,115 @@ struct unaligned_field_base {
 };
 
 struct unaligned_u8: public unaligned_field_base<1> {
-	u8 load() const { return bytes[0]; }
+	constexpr u8 load_raw() const { return bytes[0]; }
 };
+
+static constexpr u8 byte_bits = CHAR_BIT;
 
 template <typename T>
 struct load_unaligned_helper: public unaligned_field_base<sizeof(T)> {
-	constexpr static const auto width = sizeof(T);
+	constexpr static size_t width = sizeof(T);
+
+	constexpr T load_raw() const {
+		T result = 0;
+		for (size_t i = 0; i < width; i++) {
+			if constexpr (width > 1) result <<= byte_bits;
+			// XXX: am I being dumb here, surely we should not need the this->. part
+			// surely bytes should be in scope as a (superclass) data member...
+			// see unaligned_u8 above, that works fine! >:(
+			result |= this->bytes[i];
+		}
+		return result;
+	}
+};
+
+struct unaligned_u16: load_unaligned_helper<uint16_t> {};
+
+template <typename T>
+struct biased: public load_unaligned_helper<std::make_unsigned_t<T>> {
+private:
+	static_assert(
+		std::is_signed<T>::value,
+		"biased representation only makes sense for signed integrals.");
+
+	using raw_type = std::make_unsigned_t<T>;
+	// ugh, where's my damn super keyword already, it's unambiguous damnit!
+	using super = load_unaligned_helper<raw_type>;
+	constexpr static auto width = super::width;
+	constexpr static unsigned bias = 2 << ((width * byte_bits) - 1);
+public:
+
+	constexpr T load() const {
+		raw_type value = this->load_raw();
+		// do the wrap into negative integers so as to ensure it's always legal.
+		// signed wrap-around wasn't until C++20 AFAIK.
+		value -= bias;
+		return static_cast<T>(value);
+	}
+};
+
+struct joystick {
+	biased<i8> x;
+	biased<i8> y;	// NB: +Y is down
 };
 
 
 
+struct main_report {
+	// NB: report ID not specified here, it's handled elsewhere.
+	joystick joy_left;
+	joystick joy_right;
+};
+static_assert(alignof(main_report) == 1, "packed report struct not single byte alignment.");
+static_assert(sizeof(main_report) <= 63, "packed report struct is longer than the report body.");
 
 
-// NB: this technically means we have a useless NUL here... oh well
-// even more NB: be careful with mutable strings here;
-// this is fine because we've declared the *storage* non-const,
-// but doing the same with a pointer would cause Problems™.
-  static char imu_showbuf[26] = "# gyro +0000 +0000 +0000\n";
-//static char imu_showbuf[26] = "# gyro   +00   +00   +00\n";
+
+
+
+
+
+
+
+
+
+
+
+
+constexpr bool is_negative(i8 in) {
+	u8 raw = static_cast<u8>(in);
+	return (raw >> 7) == 1;
+}
+constexpr u8 abs(i8 in) {
+	u8 raw = static_cast<u8>(in);
+	return (~raw) + 1;
+}
+inline void show_biased_i8(const biased<i8> in, char* buf4) {
+	const i8 uv = in.load();
+	buf4[0] = ' ';
+	buf4[1] = ((is_negative(uv)) ? '-' : '+');
+	u8 v = abs(uv);
+	tohex(v, &buf4[2]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+static char joystick_l_showbuf[] = "# joystick  left .00 .00\n";
+
+template <typename T, size_t N>
+constexpr size_t sz(const T(&)[N]) {
+	return N;
+}
 
 int main(int argc, char** argv) {
 
@@ -149,22 +245,30 @@ int main(int argc, char** argv) {
 		ssize_t size = read(fd, buf, BUFSZ);
 		if (size < 0) return bail("# !read\n", 8);
 
+		/*
 		static char hexbuf[BUFSZ * 3];
 		size_t outsize = hexline(buf, hexbuf, size);
 		ssize_t written = write(1, hexbuf, outsize);
 		if (written < 0) return bail("# !write\n", 9);
 		if ((size_t)written != outsize) return bail("# !write:short\n", 15);
+		*/
 
 		if ((size == 64) && (buf[0] == '\x01')) {
+			const auto& report = *(const main_report*)(&buf[1]);
+			show_biased_i8(report.joy_left.x, &joystick_l_showbuf[16]);
+			show_biased_i8(report.joy_left.y, &joystick_l_showbuf[20]);
+			(void)write(1, joystick_l_showbuf, sz(joystick_l_showbuf));
+
+			/*
+			static char imu_showbuf[26] = "# gyro +0000 +0000 +0000\n";
+
 			char* showbuf = imu_showbuf;
 			show_s16le(&buf[13], &showbuf[6]);
 			show_s16le(&buf[15], &showbuf[12]);
 			show_s16le(&buf[17], &showbuf[18]);
-			//show_s8le(buf[14], &showbuf[8]);
-			//show_s8le(buf[16], &showbuf[14]);
-			//show_s8le(buf[18], &showbuf[20]);
 			ssize_t written = write(1, showbuf, 25);
 			(void)written;
+			*/
 		}
 	}
 }
